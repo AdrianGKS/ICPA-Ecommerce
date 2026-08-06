f6a38780627512a1d3e10af60a6b8b34e9488cf9
Adrian <adrian.gabriel.dev@gmail.com>
2026-08-06 12:06:35 -0300

    refactor:: Update S3 configuration and properties for improved storage management

--- backend/src/main/java/com/api/ICPAEcommerce/infra/s3/S3CloudStorageProvider.java
+++ backend/src/main/java/com/api/ICPAEcommerce/infra/s3/S3CloudStorageProvider.java
@@ -3,7 +3,6 @@ package com.api.ICPAEcommerce.infra.s3;
 import com.api.ICPAEcommerce.domain.file.FileReference;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.awscore.AwsRequestOverrideConfiguration;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -22,26 +21,17 @@ public class S3CloudStorageProvider implements CloudStorageProvider {
 
     @Override
     public URL generatePresignedUploadUrl(FileReference fileReference) {
-        var builder = AwsRequestOverrideConfiguration.builder();
-
-        if (fileReference.isPublicAccessible()) {
-            builder.putRawQueryParameter("x-amz-acl", "public-read");
-        }
-
         PutObjectRequest objectRequest = PutObjectRequest.builder()
                 .bucket(getBucket())
                 .key(fileReference.getPath())
                 .contentType(fileReference.getContentType())
                 .contentLength(fileReference.getContentLength())
-                .acl(fileReference.isPublicAccessible() ? "public-read" : null)
-                .overrideConfiguration(builder.build())
                 .build();
 
         PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
                 .signatureDuration(Duration.ofMinutes(30))
                 .putObjectRequest(objectRequest)
                 .build();
-
 
         return s3Presigner.presignPutObject(presignRequest).url();
     }
--- backend/src/main/java/com/api/ICPAEcommerce/infra/s3/S3Config.java
+++ backend/src/main/java/com/api/ICPAEcommerce/infra/s3/S3Config.java
@@ -1,39 +1,59 @@
 package com.api.ICPAEcommerce.infra.s3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.net.URI;
 
 @Configuration
 public class S3Config {
 
+    // Apontando para as novas chaves exigidas pelo StorageProperties
+    @Value("${aw.storage.s3.key-id}")
+    private String accessKey;
+
+    @Value("${aw.storage.s3.key-secret}")
+    private String secretKey;
+
+    @Value("${aw.storage.s3.endpoint}")
+    private String endpoint;
+
     @Bean
-    public S3Presigner presigner (StorageProperties storageProperties) {
-        StorageProperties.S3 s3 = storageProperties.getS3();
+    public S3Client s3Client() {
+        System.out.println("[DEBUG] accessKey=" + accessKey);
+        System.out.println("[DEBUG] secretKey=" + secretKey);
+        System.out.println("[DEBUG] endpoint=" + endpoint);
 
-        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(s3.getKeyId(), s3.getKeySecret());
-        StaticCredentialsProvider staticCredentialsProvider = StaticCredentialsProvider.create(awsBasicCredentials);
-
-        return S3Presigner.builder()
-                .region(Region.of(s3.getRegion()))
-                .credentialsProvider(staticCredentialsProvider)
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.US_EAST_1)
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
+                .forcePathStyle(true)
                 .build();
     }
 
     @Bean
-    public S3Client s3Client (StorageProperties storageProperties) {
-        StorageProperties.S3 s3 = storageProperties.getS3();
+    public S3Presigner s3Presigner() {
+        S3Configuration serviceConfiguration = S3Configuration.builder()
+                .pathStyleAccessEnabled(true)
+                .build();
 
-        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(s3.getKeyId(), s3.getKeySecret());
-        StaticCredentialsProvider staticCredentialsProvider = StaticCredentialsProvider.create(awsBasicCredentials);
-
-        return S3Client.builder()
-                .region(Region.of(s3.getRegion()))
-                .credentialsProvider(staticCredentialsProvider)
+        return S3Presigner.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.US_EAST_1)
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
+                .serviceConfiguration(serviceConfiguration)
                 .build();
     }
 }
